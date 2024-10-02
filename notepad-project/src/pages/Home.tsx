176e85a2b6862a8beafae06c1cba99795eb6e171
jjyy0804 <jjyy0804@naver.com>
2024-10-02 16:47:46 +0900

    레이아웃 작업 및 라우터 설정, 로그인 기능

---
+++ notepad-project/src/pages/Home.tsx
@@ -0,0 +1,29 @@
+import { useNavigate } from 'react-router-dom';
+import { logInWithGoogle } from '../services/authService'
+const Home = () => {
+  const navigate=useNavigate()
+;  const handleLoginClick = async () => {
+    try {
+      const user = await logInWithGoogle();
+      console.log('로그인 성공', user);
+      navigate('/notepad')
+
+    } catch(error) {
+      console.log('로그인 실패', error);
+    }
+  }
+  return (
+    <div>
+      <div className='min-h-lvh flex flex-col justify-center items-center gap-4 '>
+        <h1 className='font-bold text-indigo-500 text-2xl'>Note Pad</h1>
+        <button
+          className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
+          onClick={handleLoginClick}>구글로 로그인</button>
+      </div>
+
+    </div>
+  )
+}
+
+
+export default Home
---
+++ notepad-project/src/services/authService.ts
@@ -0,0 +1,13 @@
+import React from "react";
+import {signInWithPopup,signOut} from 'firebase/auth'
+import{auth,googleProvider} from '../firebaseConfig'
+
+export const logInWithGoogle=async () => {
+  try{
+    const result=await signInWithPopup(auth,googleProvider)
+    return result.user;
+  }catch(error)  {
+    console.log('로그인 에러',error)
+  }
+}
+
